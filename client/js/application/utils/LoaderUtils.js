5b97fbc737ba91f64fa804706da47b2c353a642c
oskar <oskar.asbrink@gmail.com>
2024-12-15 17:46:37 +0100

    path...

--- client/js/application/utils/LoaderUtils.js
+++ client/js/application/utils/LoaderUtils.js
@@ -2,10 +2,14 @@ import {GLTFLoader} from "../../../../libs/jsm/loaders/GLTFLoader.js";
 import {ImageLoader} from "../../../../libs/three/Three.Core.js";
 
 
+let path = './data/'
+if (window.islocal === true) {
+//    path = './data/'
+}
 
 let loaders = {}
-loaders['glb'] = new GLTFLoader().setPath('/data/');
-loaders['png'] = new ImageLoader().setPath('/data/');
+loaders['glb'] = new GLTFLoader().setPath(path);
+loaders['png'] = new ImageLoader().setPath(path);
 
 function getLoader(fileType) {
 
