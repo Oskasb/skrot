374b40c40a107362d2cf954165e6cb5147bb7a1a
oskar <oskar.asbrink@gmail.com>
2024-12-30 14:29:47 +0100

    wip try ssr pass

--- client/js/3d/three/fx/ThreeBloom.js
+++ client/js/3d/three/fx/ThreeBloom.js
@@ -1,6 +1,16 @@
-import {PostProcessing} from "../../../../../libs/three/Three.WebGPU.js";
-import {emissive, mrt, output, pass} from "../../../../../libs/three/Three.TSL.js";
+import {NearestFilter, PostProcessing} from "../../../../../libs/three/Three.WebGPU.js";
+import {
+    blendColor,
+    emissive,
+    metalness,
+    mrt,
+    output,
+    pass,
+    transformedNormalView
+} from "../../../../../libs/three/Three.TSL.js";
 import {bloom} from "../../../../../libs/jsm/tsl/display/BloomNode.js";
+import {ssr} from "../../../../../libs/jsm/tsl/display/SSRNode.js";
+import {smaa} from "../../../../../libs/jsm/tsl/display/SMAANode.js";
 
 class ThreeBloom{
     constructor() {
@@ -19,24 +29,45 @@ class ThreeBloom{
 
             const bloomPass = bloom( emissivePass, 1.4, 1.05, 0 );
             let postProcessing = new PostProcessing( renderer );
-            postProcessing.outputNode = outputPass.add( bloomPass );
-
-        //    renderer.setAnimationLoop( render );
-
-            function render() {
-
-                postProcessing.render();
-
-            }
 
 
+
+            const ssrNode = initSSR(outputPass, scene, camera)
+        //    outputPass.add( ssrNode );
+           postProcessing.outputNode = outputPass.add( bloomPass );
+            //    postProcessing.outputNode = outputPass.add( ssrNode );
             ThreeAPI.addPostProcess(postProcessing)
-
-
-
         }
 
+        function initSSR(outputPass, scene, camera) {
+
+            const scenePass = pass( scene, camera, { minFilter: NearestFilter, magFilter: NearestFilter } );
+            scenePass.setMRT( mrt( {
+                output: output,
+                normal: transformedNormalView,
+                metalness: metalness
+            } ) );
+
+            const scenePassColor = scenePass.getTextureNode( 'output' );
+            const scenePassNormal = scenePass.getTextureNode( 'normal' );
+            const scenePassDepth = scenePass.getTextureNode( 'depth' );
+            const scenePassMetalness = scenePass.getTextureNode( 'metalness' );
+
+            const ssrPass = ssr( scenePassColor, scenePassDepth, scenePassNormal, scenePassMetalness, camera );
+            ssrPass.resolutionScale = 1.0;
+            ssrPass.maxDistance.value = 1;
+            ssrPass.opacity.value = 1;
+            ssrPass.thickness.value = 0.03;
+            // blend SSR over beauty
+
+            return smaa( blendColor( scenePassColor, ssrPass ) );
+        //    outputPass.add(outputNode)
+        }
+
+
         this.call = {
+
+
             initBloom:initBloom
         }
 
