3c2ef6357353429db66f90f0abdd59b89a7c96a7
oskar <oskar.asbrink@gmail.com>
2025-01-04 09:50:40 +0100

    cloud colors

--- client/js/3d/three/environment/EnvironmentClouds.js
+++ client/js/3d/three/environment/EnvironmentClouds.js
@@ -2,19 +2,20 @@ import {BoxGeometry} from "../../../../../libs/three/geometries/BoxGeometry.js";
 import {SpriteNodeMaterial, VolumeNodeMaterial} from "../../../../../libs/three/materials/nodes/NodeMaterials.js";
 import {BackSide, LinearFilter, RedFormat, SRGBColorSpace} from "../../../../../libs/three/constants.js";
 import {
+    add,
     Break,
     Fn,
     If,
     instancedBufferAttribute, instanceIndex,
-    materialReference,
+    materialReference, max, min, mix, positionLocal,
     smoothstep, time, uniform,
-    vec3
+    vec3, vec4
 } from "../../../../../libs/three/Three.TSL.js";
 import {
     Color,
     Data3DTexture,
     InstancedBufferAttribute,
-    Mesh, Sprite,
+    Mesh, Sprite, Texture,
     TextureLoader,
     Vector3
 } from "../../../../../libs/three/Three.Core.js";
@@ -24,16 +25,47 @@ import {loadImageAsset} from "../../../application/utils/DataUtils.js";
 class EnvironmentClouds {
     constructor(store) {
         let scene = store.scene;
+        let camera = store.camera;
         let envUnifs = store.env.uniforms;
 
+        const sunColor = uniform( envUnifs.sun );
+        const fogColor = uniform( envUnifs.fog );
+        const ambColor = uniform( envUnifs.ambient );
 
-
-        const count = 10000;
+        const count = 1600;
 
         const positions = [];
 
         for ( let i = 0; i < count; i ++ ) {
-            positions.push( 10000 * Math.random() - 5000, 500 * Math.random() + 1000, 10000 * Math.random() - 5000 );
+            let x = 100000 * Math.random() - 50000;
+            let y = 7500 * Math.random() * Math.random() + 3000;
+            let z = 100000 * Math.random() - 50000
+            positions.push( x, y, z);
+            i++
+            x += 300 * Math.random() - 150;
+            y += 100 * Math.random() - 20;
+            z += 300 * Math.random() - 150;
+            positions.push( x, y, z);
+            i++
+            x += 300 * Math.random() - 150;
+            y += 500 * Math.random() * Math.random() - 20;
+            z += 300 * Math.random() - 150;
+            positions.push( x, y, z);
+            i++
+            x += 200 * Math.random() - 100;
+            y += 600 * Math.random() * Math.random()  - 40;
+            z += 200 * Math.random() - 100;
+            positions.push( x, y, z);
+            i++
+            x += 300 * Math.random() - 150;
+            y += 100 * Math.random() - 250;
+            z += 300 * Math.random() - 150;
+            positions.push( x, y, z);
+            i++
+            x += 300 * Math.random() - 150;
+            y += 100 * Math.random() * Math.random() - 250;
+            z += 300 * Math.random() - 150;
+            positions.push( x, y, z);
         }
 
         const positionAttribute = new InstancedBufferAttribute( new Float32Array( positions ), 3 );
@@ -42,29 +74,45 @@ class EnvironmentClouds {
 
     //    let map = new TextureLoader().load( 'textures/sprites/snowflake1.png' );
 
-        function tx1Loaded(map) {
-            map.colorSpace = SRGBColorSpace;
-
-            console.log("tx Map ", map);
-
-            let material = new SpriteNodeMaterial( { sizeAttenuation: true, map, alphaMap: map, alphaTest: 0.1, transparent: true } );
-            material.color.setHSL( 1.0, 0.3, 0.7, SRGBColorSpace );
-            material.positionNode = instancedBufferAttribute( positionAttribute );
-            material.rotationNode = time.add( instanceIndex ).sin();
-            material.scaleNode = uniform( 15 );
-
-            // sprites
-
-            const particles = new Sprite( material );
-            particles.frustumCulled = false;
-            particles.count = count;
-            scene.add( particles );
-
-
+        const texture = new Texture();
+        texture.generateMipmaps = false;
+        function tx1Loaded(image) {
+            texture.colorSpace = SRGBColorSpace;
+            texture.source.data = image;
+            texture.flipY = false;
+            texture.needsUpdate = true;
         }
+        loadImageAsset('cloud_img', tx1Loaded)
 
-        loadImageAsset('ref_sphere_5', tx1Loaded)
 
+
+
+        let material = new SpriteNodeMaterial( {
+            sizeAttenuation: true, texture, alphaMap: texture, alphaTest: 0.02, transparent: true } );
+
+        material.colorNode = Fn( () => {
+            const posx = positionLocal.x
+            const posy = positionLocal.y
+            const posz = positionLocal.z
+            const mod = posx.add(instanceIndex.mul(2).add(posx.mul(3))).sin().abs()
+            const ambShade = mix( ambColor, fogColor, posy.pow(mod.mul(7)).mul(0.5).add(0.5));
+            const sunShade = mix(ambShade, sunColor,  posy.pow(0.9));
+            return sunShade;
+        })()
+
+        material.color = store.env.fog.fog.color;
+        material.depthTest = true;
+        material.depthWrite = false;
+        material.positionNode = instancedBufferAttribute( positionAttribute );
+        material.rotationNode = time.add( instanceIndex.mul(2.1) ).sin().mul(0.07);
+        material.scaleNode = time.add( instanceIndex.mul(2.4) ).sin().mul(0.08).add(1.1).mul(5200);
+
+        // sprites
+
+        const particles = new Sprite( material );
+        particles.frustumCulled = false;
+        particles.count = count;
+        scene.add( particles );
 
     }
 
