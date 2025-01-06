b70072ec4f23736a5eec8b54c71aab148731aee7
oskar <oskar.asbrink@gmail.com>
2025-01-06 10:06:26 +0100

    env settings

--- client/js/3d/three/water/Ocean.js
+++ client/js/3d/three/water/Ocean.js
@@ -110,37 +110,32 @@ class Ocean {
 
             } )();
 
-        //    const computeTx = computeTexture( { storageTexture } ).compute( width * height );
-        //    renderer.computeAsync( computeTx );
-
                 waterMaterial.lights = true;
                 waterMaterial.colorNode = Fn( () => {
 
                     const posx = positionLocal.x
                     const posy = positionLocal.y
-                    const waveA = cos(add(add(1, add(posx, posy)), posx).mul(0.0009));
-                    const waveB = sin(add(add(1, mul(add(posx, posy), 0.9)), posy).mul(0.0006));
+                    const waveA = cos(add(add(1, add(posx, posy)), posx).mul(0.0005));
+                    const waveB = sin(add(add(1, mul(add(posx, posy), 0.9)), posy).mul(0.0003));
 
-                    const waveAp = waveA.add(cos(posy.mul(0.0004)));
-                    const waveBp = waveB.add(sin(posx.mul(0.001)));
+                    const waveAp = waveA.add(cos(posy.mul(0.0003)));
+                    const waveBp = waveB.add(sin(posx.mul(0.0005)));
                     const bigWaveNm = vec3(waveAp, 1, waveBp).normalize();
 
                     const sunColor = uniform( envUnifs.sun );
                     const fogColor = uniform( envUnifs.fog );
                     const ambColor = uniform( envUnifs.ambient );
 
-                    const waterColor = vec3(0.3, 0.4, 0.9).mul(sunColor);
-                    const blendColor = mix(waterColor, ambColor, bigWaveNm.x.abs());
-                    const blend2Color = mix(blendColor, fogColor, bigWaveNm.z.abs());
+                    const waterColor = vec3(0.4, 0.7, 0.99).mul(sunColor);
+                    const blendColor = mix(waterColor, ambColor.mul(0.7), bigWaveNm.x.mul(0.3));
+                    const blend2Color = mix(blendColor, fogColor, bigWaveNm.z.mul(0.3));
                     return blend2Color
 
                 } )();
 
-
-       //     waterMaterial.colorNode = texture( storageTexture );
-            waterMaterial.metalness = 0.97;
-            waterMaterial.envMapIntensity = 0.99;
-            waterMaterial.roughness = 0.17;
+            waterMaterial.metalness = 1.3;
+            waterMaterial.envMapIntensity = 1.99;
+            waterMaterial.roughness = 0.32;
 
             waterMaterial.positionNode = Fn( () => {
                 const { camPos } = effectController;
@@ -163,7 +158,7 @@ class Ocean {
                 const camOffsetPos = vec3(floor(camPos.x.div(TILE_SIZE)).mul(TILE_SIZE), 0 , floor(camPos.z.div(TILE_SIZE)).mul(TILE_SIZE));
                 const globalPos = vec3(pX, 0, pZ).add(camOffsetPos);
 
-                const height = time.add(pX.sub(camOffsetPos.x).mul(bnd.z.mul(3))).sin().add(pZ.sub(camOffsetPos.z).mul(bnd.z.mul(2))).cos().mul(2);
+                const height = time.add(pX.sub(camOffsetPos.x).mul(bnd.z.mul(3))).sin().add(pZ.sub(camOffsetPos.z).mul(bnd.z.mul(2))).cos().mul(1.3);
               varyingProperty( 'vec3', 'v_normalView' ).assign( vec3(1, 1, height.mul(centerNess)).normalize()  );
                 //       varyingProperty( 'vec3', 'v_normalView' ).assign( vec3(1, 1, 0).normalize()  );
                 return vec3( globalPos.x.add(edgeX), globalPos.z.add(edgeZ), height.mul(centerNess));
